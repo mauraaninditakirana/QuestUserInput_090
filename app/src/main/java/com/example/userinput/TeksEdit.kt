a47177458bacf73f3187e9b5c57a9415bc02cfa1
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-30 12:31:12 +0700

    Menambah tombol Submit dan Divider pada UI

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -96,5 +97,17 @@ fun FormDataDiri(modifier: Modifier
             jenis=textJK
             alamat=textAlamat
         }
-    ){}
+    ){
+        Text(StringResource("Submit"))
+    }
+
+    Divider(
+        modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),
+            top = dimensionResource(R.dimen.padding_medium
+            )),
+        thickness = dimensionResource(R.dimen.divider_tipis),
+        color = Color.DarkGray
+    )
+
+
 }
