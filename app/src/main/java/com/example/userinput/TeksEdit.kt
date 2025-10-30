25ff6f5955df2c5e9b3072cbe3f5a9a76a1cd691
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-30 12:16:38 +0700

    Menambah Button dan Divider pada UI.

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,8 +1,13 @@
 package com.example.userinput
 
+import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -13,23 +18,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
-import java.lang.reflect.Modifier
+
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
-    //Variabel" untuk mengingat nilai masukan dari keyboard
-    var textNama by remember { mutableStateOf("")}
-    var textAlamat by remember { mutableStateOf("")}
-    var textJK by remember { mutableStateOf("")}
+    // variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
-    //Variabel" untuk menyimpan data yang diperoleh dari komponen UI
-    var nama by remember {mutableStateOf("")}
-    var alamat by remember {mutableStateOf("")}
-    var jenis by remember {mutableStateOf("")}
+    //Variabel Untuk Menyimpan data yang diperoleh dari komponen ui
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 
-    val gender:List<String> = listOf("Laki-laki", "Perempuan")
+    val gender: List<String> = listOf("Laki-Laki","Perempuan")
 
     Column(modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
@@ -37,37 +44,54 @@ fun FormDataDiri(modifier: Modifier
         OutlinedTextField(
             value = textNama,
             singleLine = true,
-            Shape = MaterialTheme.shapes.large,
-            modifier = Modifier.width(250.dp),
-            label = { Text(text = "Nama Lengkap")},
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width = 250.dp),
+            label = { Text(text = "Nama Lengkap") },
             onValueChange = {
                 textNama = it
             }
         )
-        Row {
-            gender.forEach { item ->
-                Row(modifier = Modifier.selectable(
+    }
+    Row {
+        gender.forEach { item ->
+            Row(modifier = Modifier.selectable(
+                selected = textJK == item,
+                onClick = {textJK = item}
+            ), verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(
                     selected = textJK == item,
-                    onClick = { textJK = item }
-                ), verticalAligmnent = Aligment.CenterVertically) {
-                    RadioButton(
-                        selected = textJK == item,
-                        onClick = {
-                            textJK = item
-                        })
-                    Text(text = item)
-                }
+                    onClick = {
+                        textJK = item
+                    })
+                Text(text = item)
             }
         }
-        OutlinedTextField(
-            value = textAlamat,
-            singleLine = true,
-            modifier = Modifier.width(250.dp),
-            label = {Text{text = "Alamat Lengkap"}},
-            onValueChange = {
-                textAlamat = it
-            }
-        )
     }
-}
+    OutlinedTextField(
+        value = textAlamat,
+        singleLine =true,
+        modifier = Modifier.width(250.dp),
+        label ={Text("Alamat Lengkap")},
+        onValueChange = {
+            textAlamat = it
+        }
 
+    )
+    Divider(
+        modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),
+            top = dimensionResource(R.dimen.padding_medium
+            )),
+        thickness = dimensionResource(R.dimen.divider_tipis),
+        color = Color.DarkGray
+    )
+    Button(
+        modifier = Modifier.fillMaxWidth(1f),
+        //the button is enabled when user makes a selection
+        enabled = textAlamat.isNotEmpty(),
+        onClick = {
+            nama=textNama
+            jenis=textJK
+            alamat=textAlamat
+        }
+    )
+}
